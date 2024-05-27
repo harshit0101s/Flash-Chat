779de4c047bb898b9facc1efe928d5ccf461c6a3
Harshit Sharma <harshitsharma@Harshits-MacBook-Air.local>
2024-05-27 14:30:01 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid?
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCDFsJG4-bLsKU7FYyi_qYS7_XGXzi4CIU",
+      appId: "1:627522509057:android:31756ca60ff93910ba2300",
+      messagingSenderId: "627522509057",
+      projectId: "flash-chat-ba7aa",
+    ),
+  )
+  :await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
